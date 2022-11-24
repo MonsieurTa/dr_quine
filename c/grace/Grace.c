0d3d19eb1122a45edd73f950a59d5728dc2e11e4
William Ta <willt29000@gmail.com>
2022-11-24 14:44:11 +0100

    chore: fix makefile

--- c/grace/Grace.c
+++ c/grace/Grace.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
-#define str "#include <stdio.h>%1$c#define str %2$c%3$s%2$c%1$c#define FT()int main(){FILE *fp=fopen(%2$c./Grace_kid.c%2$c, %2$cw%2$c);fp&&fprintf(fp,str,0xa,0x22,str);}%1$cFT()%1$c"
-#define FT()int main(){FILE *fp=fopen("./Grace_kid.c", "w");fp&&fprintf(fp,str,0xa,0x22,str);}
+#include <stdlib.h>
+#define str "#include <stdio.h>%1$c#include <stdlib.h>%1$c#define str %2$c%3$s%2$c%1$c#define FT()int main(){FILE *fp=fopen(%2$c./Grace_kid.c%2$c, %2$cw%2$c);if(fp==NULL)exit(0);else fprintf(fp,str,0xa,0x22,str);}%1$cFT()%1$c"
+#define FT()int main(){FILE *fp=fopen("./Grace_kid.c", "w");if(fp==NULL)exit(0);else fprintf(fp,str,0xa,0x22,str);}
 FT()
